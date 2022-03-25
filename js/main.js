755de193cae15bdc762f89580dfa14253a218fe8
新本弘道 <>
2022-03-25 12:48:50 +0900

    スクロールの修正を致しました

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 $(function(){
   $('.scroll_btn').click(function(){
-    var speed = 500;
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
-    return false;
+    $('body,html').animate  ({
+      scrollTop: 0
+    },500);
   });
 });
