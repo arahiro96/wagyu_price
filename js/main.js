9873aeaa2332f014f3d0dade06168be25245cd9a
新本弘道 <>
2022-03-25 12:38:15 +0900

    jQueryを修正し、実装いたしました

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(function(){
-  $('a[href^="#"]').click(function(){
-    let speed = 500;
-    let href= $(this).attr("href");
-    let target = $(href == "#" || href == "" ? 'html' : href);
-    let position = target.offset().top;
+  $('.scroll_btn').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
     $("html, body").animate({scrollTop:position}, speed, "swing");
     return false;
   });
